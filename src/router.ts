381505b93bcb485c11afeec09f1c56757110b034
Brunonavarrooficial <bnacessoria@gmail.com>
2023-06-01 04:34:53 -0300

    create middlewares validate errors and movies

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "./controllers/movieControllers";
 
+import { validate } from "./middleware/handleValidation";
+import { movieCreateValidation } from './middleware/movieValidation'
+
 const router = Router()
 
 export default router
     .get('/test', (req: Request, res: Response) => {
         res.status(200).send('Api Working!')
     })
-    .post('/movie', createMovie)
+    .post('/movie', movieCreateValidation(), validate, createMovie)
